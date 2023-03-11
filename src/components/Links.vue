363b93f486ccee3d548370c4f4125f40df924b6c
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-11 17:29:16 -0300

    update dynamic state of copy button

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -4,13 +4,15 @@ import { useLocalStorage } from '@vueuse/core'
 export default {
     data() {
         return {
-            links: useLocalStorage("links", [])
+            links: useLocalStorage("links", []),
+            linkWasCopied: null,
         }
     },
     methods: {
-        writeToClipboard(text) {
+        writeToClipboard(text, index) {
             navigator.clipboard
                 .writeText(text)
+            this.linkWasCopied = index
         }
     }
 }
@@ -18,17 +20,20 @@ export default {
 
 <template>
     <div>
-        <div v-for="link in links" class="card card--link">
+        <div v-for="(link, index) in links" class="card card--link">
             <span class="original-link">{{ link.originalLink }}</span>
             <div class="card-bottom">
                 <span class="short-link">{{ link.shortLink }}</span>
-                <button :class="['button', 'button--copy', error ? 'button--copied' : '']" @click="() => writeToClipboard(link.shortLink)">Copy</button>
+                <button :class="['button', 'button--copy', linkWasCopied==index ? 'button--copied' : '']" @click="() => writeToClipboard(link.shortLink, index)">{{linkWasCopied == index ? 'Copied!' : 'Copy'}}</button>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+    .button--copied {
+        background: var(--very-dark-blue);
+    }
     .card--link {
         margin: 1em auto;
         width: 90%;

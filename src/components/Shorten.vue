1ae233f2c5323d2f5389d94daddff52c0d74bc39
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-10 23:55:39 -0300

    added error message for empty input

--- src/components/Shorten.vue
+++ src/components/Shorten.vue
@@ -5,11 +5,17 @@ export default {
     data() {
         return {
             url: "",
-            links: useLocalStorage("links", [])
+            links: useLocalStorage("links", []),
+            error: false,
         }
     },
     methods: {
         async shorten() {
+            this.error = false;
+            if (this.url === "") {
+                this.error = true;
+                return;
+            }
             const shortLink = await this.getShortenedLink()
             const link = {
                 "shortLink": shortLink,
@@ -32,11 +38,34 @@ export default {
 <template>
     <div class="shorten">
         <div class="shorten-form">
-            <input v-model="url" type="url" placeholder="Shorten a link here..." aria-label="input for url to be shortened"
-            class="shorten-form__input">
+            <div class="shorten-form-box">
+                <input v-model="url" type="url" placeholder="Shorten a link here..." aria-label="input for url to be shortened"
+                :class="['shorten-form__input', error ? 'error-input' :'']">
+                <span v-if="error" class="error-message">Please add a link</span>
+            </div>
             <button type="button" class="button button--shorten-form" @click.prevent="shorten">
                 <span class="link link--white">Shorten It!</span>
             </button>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .error-message {
+        font-size: 16px;
+        color: red;
+        font-style: italic;
+        margin: 0 auto;
+        display: block;
+    }
+    .error-input {
+        border: 1px solid red;
+    }
+    .error-input::placeholder {
+        background: red;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        text-decoration: none;
+    }
+</style>
